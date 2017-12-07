7cb836d461c00118aba2f1167a2f474a6690a959
zg2pro <ganne@sqli.com>
2017-12-07 11:52:26 +0100

    submits the form

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -5,32 +5,61 @@ const logo = require('./logo.svg');
 
 class App extends React.Component {
 
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to the Java programmer Interview questions search engine</h2>
-        </div>
-          <p className="App-intro">
-              Your search:
-              <input value={this.keywords(window.location.search.substring(1)).join(" ")} />
-              <button>Search</button>
-          </p>
-          <p>
-              Results:<br/><br/>
-              Question 1<br/>
-              Answer 1<br/><br/>
-              Question 2<br/>
-              Answer 2
-          </p>
-      </div>
-    );
-  }
+    state: any;
 
-  keywords(str: string){
-      return str.split('=')[1].split('+');
-  }
+    constructor(props: any) {
+        super(props);
+        this.state = {value: this.keywords(window.location.search.substring(1)).join(' ')};
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <div className="App">
+                    <div className="App-header">
+                        <img src={logo} className="App-logo" alt="logo"/>
+                        <h2>Welcome to the Java programmer Interview questions search engine</h2>
+                    </div>
+                    <p className="App-intro">
+                        Your search:
+                        <input type="text" value={this.state.value} onChange={this.handleChange} name="q" />
+                        <input type="submit" value="Search"/>
+                    </p>
+                    <p className="left">
+                        Results:<br/><br/>
+                        Question 1<br/>
+                        Answer 1<br/><br/>
+                        Question 2<br/>
+                        Answer 2
+                    </p>
+                </div>
+            </form>
+        );
+    }
+
+    keywords(str: string) {
+        if (str === undefined) {
+            return [''];
+        }
+        if (str.indexOf('=') > -1) {
+            str = str.split('=')[1];
+        }
+        if (str.indexOf('+') > -1) {
+            return str.split('+');
+        }
+        return [str];
+    }
+
+    handleChange(event: any) {
+        this.setState({value: event.target.value});
+    }
+
+    handleSubmit(event: any) {
+        event.target.value = event.target.value.replace(' ', '+');
+    }
 
 }
 
